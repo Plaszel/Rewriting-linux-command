45f9f69b99b887b8912fb3bec71caba637889d21
Plaszel <118224914+Plaszel@users.noreply.github.com>
2025-12-08 23:50:59 +0100

    a

--- my shell/src/miv.c
+++ my shell/src/miv.c
@@ -34,15 +34,17 @@ void appendChar( struct output * temp, char toadd)
 // moving cursor to position and updating its position variable
 void move_cursor(struct cursor * temp, int x , int y)
 {
-    int len = snprintf(NULL,0,"\x1b[%d;%dH", (temp->x + x), (temp->y + y));
+    int len = snprintf(NULL,0,"\x1b[%d;%dH", (temp->y + y), (temp->x + x));
     char * str = malloc(len + 1);
-    sprintf(str,"\x1b[%d;%dH", (temp->x + x), (temp->y + y));
+    sprintf(str,"\x1b[%d;%dH", (temp->y + y), (temp->x + x));
 
     write(STDOUT_FILENO,str,strlen(str));
     free(str);
     temp->x += x;
     temp->y += y;
 };
+
+
 
 
 //disabling  canon mode , echoing , and ctr-s /ctr-x
@@ -53,6 +55,9 @@ void disable_termosik()
     termosik_nowy = termosik_stary;
     termosik_nowy.c_lflag &= ~(ICANON | ECHO);
     termosik_nowy.c_iflag &= ~(IXON);
+    termosik_nowy.c_oflag &= ~(OPOST);
+
+    write(STDOUT_FILENO,"\x1b[?25h",strlen("\x1b[?25h"));
     tcsetattr(STDIN_FILENO,TCSANOW,&termosik_nowy);
 }
 
@@ -75,15 +80,30 @@ void main()
     {   
         //clear terminal -> show cursor -> got to home pos -> display buffered text -> if valid input add char to buffer
         write(STDOUT_FILENO,"\x1b[2J",strlen("\x1b[2J"));
-        write(STDOUT_FILENO,"\x1b[?25h",strlen("\x1b[?25h"));
         write(STDOUT_FILENO,"\x1b[1;1H",strlen("\x1b[1;1H"));
         write(STDOUT_FILENO,out.text,out.lenght);
+        move_cursor(&pos,0,0);
+        
         read(STDIN_FILENO, &c, 1); 
-        if ( c == 19) break;
+        if ( c == 19) 
+        {
+            write(STDOUT_FILENO,"\x1b[2J",strlen("\x1b[2J")); 
+            break;
+        }
+        if ( c == 10)
+        {
+            move_cursor(&pos,(-pos.x),1);
+            appendChar(&out,'\n');
+            continue;
+        }
         appendChar(&out,c);
+        move_cursor(&pos,1,0);
         
     }
-
+    FILE * f = fopen("test.txt","w");
+    fprintf(f,out.text);
+    fclose(f);
+    free(out.text);
     enable_termosik();
 
 }
